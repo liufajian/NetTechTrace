8ce119439234edcd1f8e0652b936110bd86daa72
liufajian <379002060@qq.com>
2022-04-06 21:24:20 +0800

    TestEnumerator

--- NetCoreTest/System.Collections/BasicTest.cs
+++ NetCoreTest/System.Collections/BasicTest.cs
@@ -28,5 +28,45 @@ namespace NetCoreTest.System.Collections
             nv.Set(key1, "444");
             Assert.AreEqual(nv[key1], "444");
         }
+
+        [TestMethod]
+        public void TestEnumerator()
+        {
+            var list = new List<int> { 11, 22, 33 };
+            var enm = list.GetEnumerator();
+            
+            Assert.AreEqual(enm.Current, 0);
+            
+            Assert.IsTrue(enm.MoveNext());
+            Assert.AreEqual(enm.Current, 11);
+            
+            Assert.IsTrue(enm.MoveNext());
+            Assert.AreEqual(enm.Current, 22);
+            
+            Assert.IsTrue(enm.MoveNext());
+            Assert.AreEqual(enm.Current, 33);
+
+            Assert.IsFalse(enm.MoveNext());
+            Assert.IsFalse(enm.MoveNext());
+
+            //----------------------------------
+
+            var list2 = new List<string> { "a1", "a2", "a3" };
+            var enm2 = list2.GetEnumerator();
+
+            Assert.AreEqual(enm2.Current, default(string));
+
+            Assert.IsTrue(enm2.MoveNext());
+            Assert.AreEqual(enm2.Current, "a1");
+
+            Assert.IsTrue(enm2.MoveNext());
+            Assert.AreEqual(enm2.Current, "a2");
+
+            Assert.IsTrue(enm2.MoveNext());
+            Assert.AreEqual(enm2.Current, "a3");
+
+            Assert.IsFalse(enm2.MoveNext());
+            Assert.IsFalse(enm2.MoveNext());
+        }
     }
 }
---
+++ ThirdPartyLibTest/NpoiTest/TestDocReplace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdPartyLibTest.NpoiTest
+{
+    [TestClass]
+    public class TestDocReplace
+    {
+        [TestMethod]
+        public void Test1()
+        {
+
+        }
+    }
+}
